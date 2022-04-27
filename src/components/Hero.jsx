78a2e895cb8bf3c2b92c1a6d4c2da321213955c7
aNd12326 <andersonventosilla0@gmail.com>
2022-04-26 19:58:21 -0500

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { 
-    CloudUploadIcon, 
-    DatabaseIcon, 
+import {
+    CloudUploadIcon,
+    DatabaseIcon,
     PaperAirplaneIcon,
-    ServerIcon, 
+    ServerIcon,
 } from '@heroicons/react/solid'
 
 import bgImg from '../assets/cyber-bg.png'
@@ -20,6 +20,17 @@ const Hero = () => {
                 </div>
                 <div>
                     <img className='w-full' src={bgImg} alt='/' />
+                </div>
+                <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
+            mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
+            border border-slate-300 rounded-xl text-center shadow-xl'>
+                    <p>Data Services</p>
+                    <div className='flex justify-between flex-wrap px-4'>
+                        <p className='flex px-4 py-2 text-slate-500'><CloudUploadIcon  className='h-6 text-indigo-600'/>Icon. App Security</p>
+                        <p className='flex px-4 py-2 text-slate-500'><DatabaseIcon  className='h-6 text-indigo-600'/>Icon. Dashboard</p>
+                        <p className='flex px-4 py-2 text-slate-500'><ServerIcon  className='h-6 text-indigo-600'/>Icon. Cloud Data</p>
+                        <p className='flex px-4 py-2 text-slate-500'><PaperAirplaneIcon className='h-6 text-indigo-600'/>Icon. API</p>
+                    </div>
                 </div>
             </div>
         </div>
